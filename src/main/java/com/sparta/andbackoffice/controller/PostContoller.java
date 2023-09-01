c38889f37214f924b5ddf6236e508c1f6f3d0c02
Lee hyunkyoung <hyunk.office@gmail.com>
2023-09-01 19:51:31 +0900

    :sparkles: 백오피스 게시물 삭제

--- src/main/java/com/sparta/andbackoffice/controller/PostContoller.java
+++ src/main/java/com/sparta/andbackoffice/controller/PostContoller.java
@@ -4,9 +4,7 @@ import com.sparta.andbackoffice.dto.response.PostListResponseDto;
 import com.sparta.andbackoffice.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +17,10 @@ public class PostContoller {
     public ResponseEntity<PostListResponseDto> getPosts() {
         PostListResponseDto getPosts = postService.getPosts();
         return ResponseEntity.ok().body(getPosts);
+    }
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable Long id) {
+        String message = postService.deletePost(id);
+        return ResponseEntity.ok(message);
     }
 }
--- src/main/java/com/sparta/andbackoffice/service/PostService.java
+++ src/main/java/com/sparta/andbackoffice/service/PostService.java
@@ -2,9 +2,13 @@ package com.sparta.andbackoffice.service;
 
 import com.sparta.andbackoffice.dto.response.PostListResponseDto;
 import com.sparta.andbackoffice.dto.response.PostResponseDto;
+import com.sparta.andbackoffice.entity.Post;
 import com.sparta.andbackoffice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,4 +26,15 @@ public class PostService {
 
         return new PostListResponseDto(postList);
     }
+    //게시글 삭제 API
+    @Transactional
+    public String deletePost(Long id) {
+        Post post = PostRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."));
+
+        PostRepository.delete(post);
+
+        return "게시글이 성공적으로 삭제되었습니다.";
+    }
+
 }
