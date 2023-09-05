763f8f749723c1c448430ec08b8ddec59d887acd
harry park <harrytucker1016@gmail.com>
2023-09-05 16:47:54 +0900

    ContestStatus 스케줄러 추가 (테스트전)

--- src/main/java/com/sparta/andbackoffice/controller/ContestController.java
+++ src/main/java/com/sparta/andbackoffice/controller/ContestController.java
@@ -1,13 +1,66 @@
 package com.sparta.andbackoffice.controller;
 
+import com.sparta.andbackoffice.dto.request.ContestRequestDto;
+import com.sparta.andbackoffice.dto.response.ApiResponseDto;
+import com.sparta.andbackoffice.dto.response.ContestResponseDto;
+import com.sparta.andbackoffice.service.ContestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j(topic = "ContestController")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/contest")
 public class ContestController {
+
+    private final ContestService contestService;
+
+    // 글 작성
+    @PostMapping
+    public ResponseEntity<?> createContest(@RequestBody ContestRequestDto requestDto) {
+        log.info("Controller - createContest : 시작");
+
+        ContestResponseDto result = contestService.createContest(requestDto);
+
+        log.info("Controller - createContest : 끝");
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    // 글 단건 조회
+    @GetMapping("/{contestId}")
+    public ResponseEntity<?> getContest(@PathVariable Long contestId) {
+        log.info("Controller - getContest : 시작");
+
+        ContestResponseDto result = contestService.getContest(contestId);
+
+        log.info("Controller - getContest : 끝");
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    // 글 수정
+    @PatchMapping("/{contestId}")
+    public ResponseEntity<?> modifyContest(@PathVariable Long contestId,
+                                           @RequestBody ContestRequestDto requestDto) {
+        log.info("Controller - modifyContest : 시작");
+
+        ContestResponseDto result = contestService.modifyContest(contestId, requestDto);
+
+        log.info("Controller - modifyContest : 끝");
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+
+    }
+
+    // 글 삭제
+    @DeleteMapping("/{contestId}")
+    public ResponseEntity<?> deleteContest(@PathVariable Long contestId) {
+        log.info("Controller - deleteContest : 시작");
+
+        ApiResponseDto result = contestService.deleteContest(contestId);
+
+        log.info("Controller - deleteContest : 끝");
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
 }
--- src/main/java/com/sparta/andbackoffice/service/ContestService.java
+++ src/main/java/com/sparta/andbackoffice/service/ContestService.java
@@ -1,32 +1,116 @@
 package com.sparta.andbackoffice.service;
 
+import com.sparta.andbackoffice.dto.request.ContestRequestDto;
+import com.sparta.andbackoffice.dto.response.ApiResponseDto;
+import com.sparta.andbackoffice.dto.response.ContestResponseDto;
 import com.sparta.andbackoffice.entity.Contest;
 import com.sparta.andbackoffice.entity.ContestStatus;
 import com.sparta.andbackoffice.repository.ContestRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Service
+@EnableScheduling
 @Slf4j(topic = "ContestService")
 @RequiredArgsConstructor
 public class ContestService {
 
-    private ContestRepository contestRepository;
+	private final ContestRepository contestRepository;
 
-    public ContestStatus contestStatus(Contest contest) {
-        LocalDate currentDate = LocalDate.now();
+	public ContestResponseDto createContest(ContestRequestDto requestDto) {
+		log.info("Service - createContest : 시작");
 
-        if (currentDate.isBefore(contest.getStartDate())) {
-            return ContestStatus.UPCOMING;
-        } else if (currentDate.isEqual(contest.getStartDate()) || currentDate.isEqual(contest.getEndDate()) || currentDate.isBefore(contest.getEndDate())) {
-            return ContestStatus.ONGOING;
-        } else if (currentDate.isBefore(contest.getEndDate().minusDays(3))) {
-            return ContestStatus.CLOSING;
-        } else {
-            return ContestStatus.CLOSED;
-        }
-    }
+		Contest contest = new Contest(requestDto);
+		contest.setStatus((contestStatus(contest))); // 마감기한 코드 넣기
+		contest = contestRepository.save(contest);
+
+		log.info("Service - createContest : 끝");
+		return new ContestResponseDto(contest);
+	}
+
+	public ContestResponseDto getContest(Long contestId) {
+		log.info("Service - getContest : 시작");
+
+		Contest contest = findContest(contestId);
+
+		log.info("Service - getContest : 끝");
+		return new ContestResponseDto(contest);
+	}
+
+	@Transactional
+	public ContestResponseDto modifyContest(Long contestId, ContestRequestDto requestDto) {
+		log.info("Service - modifyContest : 시작");
+
+		Contest contest = findContest(contestId);
+
+		// Dto -> List 받아서 반복문 돌리게 하고 싶지만...
+		contest.setAuthor(requestDto.getAuthor());
+		contest.setTitle(requestDto.getTitle());
+		contest.setCompany(requestDto.getCompany());
+		contest.setEndDate(requestDto.getEndDate());
+		contest.setStartDate(requestDto.getStartDate());
+		contest.setHomepage(requestDto.getHomepage());
+		contest.setContents(requestDto.getContents());
+
+		contest.setStatus(contestStatus(contest)); //마감기한 상태코드 다시 계산해서 업데이트
+
+		log.info("Service - modifyContest : 끝");
+		return new ContestResponseDto(contest);
+	}
+
+	@Transactional
+	public ApiResponseDto deleteContest(Long contestId) {
+		log.info("Service - deleteContest : 시작");
+
+		Contest contest = findContest(contestId);
+		contestRepository.delete(contest);
+
+		log.info("Service - deleteContest : 끝");
+		return new ApiResponseDto("게시글 삭제 완료", HttpStatus.OK.value());
+	}
+
+	public Contest findContest(Long contestId) {
+		return contestRepository.findById(contestId).orElseThrow(
+				() -> new IllegalArgumentException("존재하지 않는 글입니다.")
+		);
+	}
+
+	@Scheduled(cron = "0 0 0 * * ?") //매일 자정에 실행됨
+	public void updateContestStatus(){
+		log.info("Service - updateStatus : 시작");
+
+		contestRepository.findAll() //모든 공보전을 가져오고
+				.forEach(contest -> {  // 반복 루프 ContestStatus메서드 호출해서 상태코드 설정
+					ContestStatus status = contestStatus(contest);
+					contest.setStatus(status); //contest 객체에 set
+				});
+
+		log.info("Service - updateStatus : 끝");
+	}
+
+	public ContestStatus contestStatus(Contest contest) {
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		LocalDateTime startDate = contest.getStartDate();
+		LocalDateTime endDate = contest.getEndDate();
+		LocalDateTime endDateMinus3Days = endDate.minusDays(3);
+
+		if (currentDateTime.isBefore(startDate)) {
+			return ContestStatus.UPCOMING;
+		} else if (currentDateTime.isEqual(startDate) || currentDateTime.isEqual(endDate) || currentDateTime.isBefore(endDate)) {
+			return ContestStatus.ONGOING;
+		} else if (currentDateTime.isBefore(endDateMinus3Days)) {
+			return ContestStatus.CLOSING;
+		} else {
+			return ContestStatus.CLOSED;
+		}
+	}
 }
