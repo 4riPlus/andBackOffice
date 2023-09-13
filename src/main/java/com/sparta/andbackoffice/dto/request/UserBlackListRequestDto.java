7653436363d3bc55f2c6e5428add342aecd92363
Lee hyunkyoung <hyunk.office@gmail.com>
2023-09-13 18:36:01 +0900

    :sparkles: 블랙리스트 등록 수정중

--- src/main/java/com/sparta/andbackoffice/dto/request/UserBlackListRequestDto.java
+++ src/main/java/com/sparta/andbackoffice/dto/request/UserBlackListRequestDto.java
@@ -1,8 +1,12 @@
 package com.sparta.andbackoffice.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@NoArgsConstructor
+@Setter
 public class UserBlackListRequestDto {
-    private Long userId;
+    private String userName;
 }
--- src/main/java/com/sparta/andbackoffice/dto/response/UserBlackListResponseDto.java
+++ src/main/java/com/sparta/andbackoffice/dto/response/UserBlackListResponseDto.java
@@ -1,5 +1,7 @@
 package com.sparta.andbackoffice.dto.response;
 
+import com.sparta.andbackoffice.entity.Admin;
+import com.sparta.andbackoffice.entity.User;
 import com.sparta.andbackoffice.entity.UserBlackList;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserBlackListResponseDto {
-    private Long blackListId;
+    private String message;
 
-    public UserBlackListResponseDto(UserBlackList userBlackList) {
-        this.blackListId = userBlackList.getBlackListId();
+    public UserBlackListResponseDto(String message) {
+        this.message = message;
     }
 }
