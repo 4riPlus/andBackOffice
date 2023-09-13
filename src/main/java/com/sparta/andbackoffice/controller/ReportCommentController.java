e2b1a622e72b96d98c3a010b46be4a64eafb57b3
hjern <hjern.refrain@gmail.com>
2023-09-13 21:08:52 +0900

    :sparkles: ready for pagination

--- src/main/java/com/sparta/andbackoffice/controller/ReportCommentController.java
+++ src/main/java/com/sparta/andbackoffice/controller/ReportCommentController.java
@@ -4,9 +4,12 @@ import com.sparta.andbackoffice.dto.response.ReportCommentCountResponseDto;
 import com.sparta.andbackoffice.dto.response.ReportCommentResponseDto;
 import com.sparta.andbackoffice.service.ReportCommentService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +21,10 @@ public class ReportCommentController {
 	private final ReportCommentService reportCommentService;
 
 	@GetMapping
-	public ResponseEntity<List<ReportCommentResponseDto>> getReportComments() {
-		return ResponseEntity.ok().body(reportCommentService.getReportComments());
+	public ResponseEntity<Page<ReportCommentResponseDto>> getReportComments(
+		@RequestParam("page") int page,
+		@RequestParam("size") int size) {
+		return ResponseEntity.ok().body(reportCommentService.getReportComments(page-1,size));
 	}
 
 	@GetMapping("/counts")
