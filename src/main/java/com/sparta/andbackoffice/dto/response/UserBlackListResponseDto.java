baff62cc0ef7169a48d63069eaac6e744fa1559e
Lee hyunkyoung <hyunk.office@gmail.com>
2023-09-14 18:04:30 +0900

    :memo: 유저블랙리스트 로직수정(미완성)

--- src/main/java/com/sparta/andbackoffice/dto/response/UserBlackListResponseDto.java
+++ src/main/java/com/sparta/andbackoffice/dto/response/UserBlackListResponseDto.java
@@ -5,14 +5,18 @@ import com.sparta.andbackoffice.entity.User;
 import com.sparta.andbackoffice.entity.UserBlackList;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class UserBlackListResponseDto {
     private String message;
 
     public UserBlackListResponseDto(String message) {
         this.message = message;
+    }
+    public UserBlackListResponseDto(UserBlackList userBlackList) {
     }
 }
